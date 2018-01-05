f0daa15935ade16ce0e334d9c05458b6e9058bb6
Maxine Chui <maxine.chui@gmail.com>
2018-01-05 10:37:06 -0800

    configure mailer in development.rb

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,6 +1,9 @@
 class EmailsController < ApplicationController
 
   def create
+    address = params[:to]
+    subject = params[:subject]
+    body = params[:body]
 
     message = UserMailer.send_email(address, subject, body)
     message.deliver_now

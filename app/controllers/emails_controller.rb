601dcec6188fd112ffd256cda5810ece8e08eab0
Maxine Chui <maxine.chui@gmail.com>
2018-01-05 10:26:32 -0800

    create methods for sending mail

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,2 +1,11 @@
 class EmailsController < ApplicationController
+
+  def create
+
+    message = UserMailer.send_email(address, subject, body)
+    message.deliver_now
+
+    render json: ['Message sent!']
+  end
+
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,13 @@
 class UserMailer < ApplicationMailer
+
+  default from: 'no-reply@example.com'
+
+  def send_email(address, subject, body)
+    address = params[:to]
+    subject = params[:subject]
+    body = params[:body]
+
+    mail(to: address, subject: subject, body: body)
+  end
+
 end
